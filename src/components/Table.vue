701395b8f18d38ffefeee6d9aa003ab52783f01b
bm13563 <bm13563@my.bristol.ac.uk>
2024-08-28 22:28:19 +0100

    refactor cos primevue isnt good

---
+++ src/components/Table.vue
@@ -0,0 +1,82 @@
+<template>
+  <div :class="{ hidden: !display }">
+    <DataTable :value="data" scrollable :scroll-height="tableHeight">
+      <Column
+        v-for="column in columns"
+        :key="column.field"
+        :field="column.field"
+        :header="column.header"
+        :frozen="column.field === 'player'"
+        :sortable="sortable"
+      ></Column>
+    </DataTable>
+  </div>
+</template>
+
+<script setup>
+import Column from "primevue/column"
+import DataTable from "primevue/datatable"
+
+const props = defineProps(["data", "columns", "tableHeight", "display", "sortable"])
+</script>
+
+<style scoped>
+.hidden {
+  display: none;
+}
+
+::v-deep .p-datatable {
+  max-width: 90vw;
+
+  @media (max-width: 768px) {
+    width: 90vw;
+  }
+}
+
+::v-deep .p-datatable-table {
+  font-family: "Roboto", sans-serif;
+  font-size: 18px;
+  width: 100%;
+
+  @media (max-width: 768px) {
+    font-size: 12px;
+  }
+}
+
+::v-deep .p-datatable-table > thead > tr:first-of-type > th:first-of-type {
+  border-radius: 0.5rem 0 0 0 !important;
+}
+
+::v-deep .p-datatable-table > thead > tr:first-of-type > th:last-of-type {
+  border-radius: 0 0.5rem 0 0 !important;
+}
+
+::v-deep .p-datatable-table > tbody > tr:last-of-type > td:first-of-type {
+  border-radius: 0 0 0 0.5rem !important;
+}
+
+::v-deep .p-datatable-table > tbody > tr:last-of-type > td:last-of-type {
+  border-radius: 0 0 0.5rem 0 !important;
+}
+
+::v-deep .p-datatable thead th {
+  font-size: 12px;
+}
+
+::v-deep .p-datatable thead th,
+::v-deep .p-datatable tbody td,
+::v-deep .p-datatable tfoot td {
+  padding-left: 0.75rem;
+  padding-right: 0.75rem;
+  max-width: 10rem;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+
+  @media (max-width: 768px) {
+    max-width: 5rem;
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
+  }
+}
+</style>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -3,109 +3,139 @@
     <div class="title">
       <img src="../assets/torps_logo.png" alt="logo" />
       <span class="text">
-      <h1 >Torps Fantasy League</h1>
-      <p>Last updated: {{ dateFormat(lastUpdated, "yyyy-mm-dd HH:MM") }}</p>
-    </span>
+        <h1>Torps Fantasy League</h1>
+        <p>Last updated: {{ dateFormat(lastUpdated, "yyyy-mm-dd HH:MM") }}</p>
+      </span>
     </div>
     <SelectButton v-model="tab" :options="TABS" :allowEmpty="false" class="selector" />
     <div class="actions">
       <Select v-model="source" :options="SOURCES" :allowEmpty="false" />
       <div v-if="isPlayers" class="show-extended">
         <p>Show all player data</p>
-        <ToggleSwitch v-model="showExtended" />
+        <ToggleSwitch v-model="isExtended" />
       </div>
     </div>
     <ProgressSpinner id="spinner" v-if="loading" />
-    <DataTable v-else :value="tableData" scrollable :scroll-height="tableHeight">
-      <Column
-        v-for="column in tableColumns.filter((c) => showExtended || c.default)"
-        :key="column.field"
-        :field="column.field"
-        :header="column.header"
-        :sortable="showExtended && isPlayers"
-        :frozen="column.field === 'player'"
-      ></Column>
-    </DataTable>
+    <template v-else>
+      <!-- hilarious but primevue tables are so slow that it's better not to unmount them -->
+      <Table
+        :data="firstsPlayers"
+        :columns="PLAYER_COLUMNS"
+        :tableHeight="tableHeight"
+        :display="isFirsts && isPlayers && isExtended"
+        :sortable="true"
+      />
+      <Table
+        :data="firstsPlayers"
+        :columns="PLAYER_COLUMNS.filter((column) => column.default)"
+        :tableHeight="tableHeight"
+        :display="isFirsts && isPlayers && !isExtended"
+        :sortable="false"
+      />
+      <Table
+        :data="clubPlayers"
+        :columns="PLAYER_COLUMNS"
+        :tableHeight="tableHeight"
+        :display="!isFirsts && isPlayers && isExtended"
+        :sortable="true"
+      />
+      <Table
+        :data="clubPlayers"
+        :columns="PLAYER_COLUMNS.filter((column) => column.default)"
+        :tableHeight="tableHeight"
+        :display="!isFirsts && isPlayers && !isExtended"
+        :sortable="false"
+      />
+      <Table
+        :data="firstsTeams"
+        :columns="TEAM_COLUMNS"
+        :tableHeight="tableHeight"
+        :display="isFirsts && !isPlayers"
+        :sortable="false"
+      />
+      <Table
+        :data="clubTeams"
+        :columns="TEAM_COLUMNS"
+        :tableHeight="tableHeight"
+        :display="!isFirsts && !isPlayers"
+        :sortable="false"
+      />
+    </template>
   </div>
 </template>
 
 <script setup>
-import { onBeforeMount, ref, watch, computed } from "vue"
+import { ref, watch, computed } from "vue"
 
-import DataTable from "primevue/datatable"
-import Column from "primevue/column"
 import ToggleSwitch from "primevue/toggleswitch"
 import SelectButton from "primevue/selectbutton"
 import ProgressSpinner from "primevue/progressspinner"
 import Select from "primevue/select"
 import dateFormat from "dateformat"
+import Table from "../components/Table.vue"
 
 import { useRoute, useRouter } from "vue-router"
 
 import { getPlayers, getTeams, getLastUpdated } from "../data"
+import { onMounted } from "vue"
 
 const $route = useRoute()
 const $router = useRouter()
 
-const loading = ref(true)
+// constants
+let firstsPlayers
+let clubPlayers
+let firstsTeams
+let clubTeams
+let lastUpdated
+let tableHeight
 
+// reactive
+const loading = ref(true)
 const tab = ref($route.query.tab || TABS[0])
 const source = ref($route.query.source || SOURCES[0])
+const isExtended = ref(window.innerHeight > 768)
 
-const playerData = ref([])
-const teamData = ref([])
-const lastUpdated = ref()
-const tableData = ref([])
-
-const tableColumns = ref([])
-const showExtended = ref(window.innerHeight > 768)
-
+const isFirsts = computed(() => source.value === "Firsts")
 const isPlayers = computed(() => tab.value === "Players")
-const visibleColumnsLength = computed(() => tableColumns.value.filter((c) => (showExtended.value ? true : c.default)))
-const tableHeight = computed(() => {
+
+const getTableHeight = () => {
   const bottomOfActions = document.querySelector(".actions").getBoundingClientRect().bottom
   const bottomOfPage = window.visualViewport.height
   return bottomOfPage - bottomOfActions - 32 + "px"
-})
-
-const getData = async (newSource) => {
-  playerData.value = await getPlayers(newSource.toLowerCase())
-  teamData.value = await getTeams(newSource.toLowerCase())
-  teamData.value = teamData.value.map((team, index) => ({ ...team, rank: index + 1 }))
-  const lastUpdatedUnformatted = await getLastUpdated()
-  lastUpdated.value = new Date(lastUpdatedUnformatted.lastUpdated)
-}
-
-const chooseData = (newTab) => {
-  if (newTab === "Players") {
-    tableData.value = playerData.value
-    tableColumns.value = PLAYER_COLUMNS
-  } else {
-    tableData.value = teamData.value
-    tableColumns.value = TEAM_COLUMNS
-  }
-  tableHeight.value
 }
 
 watch(tab, (newTab) => {
   loading.value = true
-  chooseData(newTab)
   $router.push({ query: { tab: newTab, source: source.value } })
   loading.value = false
 })
 
 watch(source, async (newSource) => {
   loading.value = true
-  await getData(newSource)
-  chooseData(tab.value)
   $router.push({ query: { tab: tab.value, source: newSource } })
   loading.value = false
 })
 
-onBeforeMount(async () => {
-  await getData(source.value)
-  chooseData(tab.value)
+onMounted(async () => {
+  loading.value = true
+
+  tableHeight = getTableHeight()
+
+  firstsPlayers = await getPlayers("firsts")
+  clubPlayers = await getPlayers("club")
+
+  firstsTeams = await getTeams("firsts")
+  firstsTeams = firstsTeams.map((team, index) => ({ ...team, rank: index + 1 }))
+
+  clubTeams = await getTeams("club")
+  clubTeams = clubTeams.map((team, index) => ({ ...team, rank: index + 1 }))
+
+  lastUpdated = await getLastUpdated()
+  lastUpdated = new Date(lastUpdated.lastUpdated)
+
   $router.push({ query: { tab: tab.value, source: source.value } })
+
   loading.value = false
 })
 </script>
@@ -177,62 +207,6 @@ onBeforeMount(async () => {
   display: flex;
   gap: 1rem;
   align-items: center;
-}
-
-::v-deep .p-datatable {
-  width: v-bind(visibleColumnsLength * 10 + 10 + "rem");
-  max-width: 90vw;
-
-  @media (max-width: 768px) {
-    width: 90vw;
-  }
-}
-
-::v-deep .p-datatable-table {
-  font-family: "Roboto", sans-serif;
-  font-size: 18px;
-  width: 100%;
-
-  @media (max-width: 768px) {
-    font-size: 12px;
-  }
-}
-
-::v-deep .p-datatable-table > thead > tr:first-of-type > th:first-of-type {
-  border-radius: 0.5rem 0 0 0 !important;
-}
-
-::v-deep .p-datatable-table > thead > tr:first-of-type > th:last-of-type {
-  border-radius: 0 0.5rem 0 0 !important;
-}
-
-::v-deep .p-datatable-table > tbody > tr:last-of-type > td:first-of-type {
-  border-radius: 0 0 0 0.5rem !important;
-}
-
-::v-deep .p-datatable-table > tbody > tr:last-of-type > td:last-of-type {
-  border-radius: 0 0 0.5rem 0 !important;
-}
-
-::v-deep .p-datatable thead th {
-  font-size: 12px;
-}
-
-::v-deep .p-datatable thead th,
-::v-deep .p-datatable tbody td,
-::v-deep .p-datatable tfoot td {
-  padding-left: 0.75rem;
-  padding-right: 0.75rem;
-  max-width: 10rem;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-
-  @media (max-width: 768px) {
-    max-width: 5rem;
-    padding-left: 0.5rem;
-    padding-right: 0.5rem;
-  }
 }
 </style>
 
